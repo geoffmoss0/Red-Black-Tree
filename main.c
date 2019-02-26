c3f1a336f809fe55e918909d65b6cda8e05bb31a
Geoffrey Moss <geoffmoss0@gmail.com>
2019-02-26 17:29:47 -0500

    Runs with no errors, segfault issue fixed by adding value 0 leaf nodes

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ struct node *root;
 */
 void left_rotate(struct node *node) {
 	struct node *other = node->right;
+	printf("other: %d\n", other->value);
 	node->right = other->left;
 	if (other->left != NULL) {
 		other->left->parent = node;
@@ -32,33 +33,140 @@ void left_rotate(struct node *node) {
 
 
 /**
+* Rotate the tree right
+* @param node the node to rotate around
+*/
+void right_rotate(struct node *node) {
+	struct node *other = node->left;
+	node->left = other->right;
+	if (other->right != NULL) {
+		other->right->parent = node;
+	}
+	other->parent = node->parent;
+	if (node->parent == NULL) {
+		root = other; //This operation changes the root of the tree to the right child
+	} else {
+		if (node == (node->parent)->right) {
+			node->parent->right = other;
+		} else {
+			node->parent->left = other;
+		}
+	}
+	other->right = node; //Original node is now the left child of its former right child
+	node->parent = other;
+}
+
+/**
+* repair the red-black property of the tree
+* again, just used pseudocode, haven't proved any of this
+* @param node the node inserted most recently
+*/
+void repair(struct node *node) {
+	while ( (node != root) && (node->parent->color)) { //red evaluates to true, black to false
+		if (node->parent == node->parent->parent->left) {
+
+			struct node *other = node->parent->parent->right;
+			if (other->color) { //again, red is true, black is false
+				node->parent->color = 0;
+				other->color = 0;
+				node->parent->parent->color = 1;
+				//set x to x's grandparent (this always works because we check x's uncle, 
+				//and if it's red it has to have a parent)
+				node = node->parent->parent;
+			} else { //uncle is a black node
+				if (node == node->parent->right) {
+					node = node->parent;
+					left_rotate(node);
+				}
+				node->parent->color = 0;
+				node->parent->parent->color = 1;
+				right_rotate(node->parent->parent);
+			}
+		} else { //node->parent == node->parent->parent->right
+			struct node *other = node->parent->parent->left;
+			if (other->color) { //again, red is true, black is false
+				node->parent->color = 0;
+				other->color = 0;
+				node->parent->parent->color = 1;
+				node = node->parent->parent;
+			} else { //uncle is a black node
+				if (node == node->parent->left) {
+					node = node->parent;
+					right_rotate(node);
+				}
+				node->parent->color = 0;
+				node->parent->parent->color = 1;
+				left_rotate(node->parent->parent);
+			}
+		}
+	}
+	root->color = 0;
+}
+
+
+/**
 * Recursive function that adds a node (sets parent to the next free node, 
 * gives it a value and color)
 * @param *root pointer to the root node
 * @param *node the node to be added
 */
-int insert(struct node *start, struct node *node) {
-	//TODO
-	if (node->value < start->value) {
-		if (start->left == NULL) {
-			node->parent = start;
-			start->left = node;
-			node->color = 1;
-			printf("Added node with value %d to node with value %d on left\n", node->value, start->value);
+int insert(struct node *start, int val) {
+	if (val < start->value) {
+		if (start->left->value == 0) {
+			
+			//Make the NULL leaf nodes for the node we just made a non-leaf node
+			struct node *left = malloc(sizeof(node));
+			left->parent = start->left;
+			left->right = NULL;
+			left->left = NULL;
+			left->color = 0;
+			left->value = 0;
+
+			struct node *right = malloc(sizeof(node));
+			right->parent = start->left;
+			right->right = NULL;
+			right->left = NULL;
+			right->color = 0;
+			right->value = 0;
+
+			start->left->color = 1;
+			start->left->value = val;
+			start->left->left = left;
+			start->left->right = right;
+
+			printf("Added node with value %d to node with value %d on left\n", start->left->value, start->value);
+			printf("Repairing...\n");
 			return 1;
 		} else {
-			return insert(start->left, node);
+			return insert(start->left, val);
 		}
 	
 	} else { //node->value >= start->value
-		if (start->right == NULL) {
-			node->parent = start;
-			start->right = node;
-			node->color = 1;
-			printf("Added node with value %d to node with value %d on right\n", node->value, start->value);	
+		if (start->right->value == 0) {
+
+			struct node *left = malloc(sizeof(node));
+			left->parent = start->right;
+			left->right = NULL;
+			left->left = NULL;
+			left->color = 0;
+			left->value = 0;
+
+			struct node *right = malloc(sizeof(node));
+			right->parent = start->right;
+			right->right = NULL;
+			right->left = NULL;
+			right->color = 0;
+			right->value = 0;
+
+			start->right->color = 1;
+			start->right->value = val;
+			start->right->left = left;
+			start->right->right = right;
+			printf("Added node with value %d to node with value %d on right\n", start->right->value, start->value);	
+			printf("Repairing...\n");
 			return 1;
 		} else {
-			return insert(start->right, node);
+			return insert(start->right, val);
 		}
 	}
 }
@@ -81,26 +189,36 @@ int main(void) {
 			n->parent = NULL;
 			n->value = x;
 			n->color = 0;
-			n->left = NULL;
-			n->right = NULL;
+			
+			struct node *left = malloc(sizeof(node));
+			left->parent = n;
+			left->value = 0;
+			left->color = 0;
+			left->left = NULL;
+			left->right = NULL;
+
+			struct node *right = malloc(sizeof(node));
+			right->parent = n;
+			right->value = 0;
+			right->color = 0;
+			right->right = NULL;
+			right->left = NULL;
+
+			n->left = left;
+			n->right = right;
 			root = n;
 		} else {
-			struct node *n = malloc(sizeof(node));
-			n->value = x;
-			n->parent = NULL;
-			n->left = NULL;
-			n->right = NULL;
-			if (!insert(root, n)) {
+			if (!insert(root, x)) {
 				printf("Something's gone horribly wrong");
 				exit(1);
 			}
 		}
-		//TODO replace this with the insert and balancing methods
 		count++;
 	}
 
 	printf("\n");
 
 	printf("root value: %d\n", root->value);
+
 	return 0;
 }
