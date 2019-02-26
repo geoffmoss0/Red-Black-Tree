498ec1a7d07f6ccc3bc3f214cc09d16a191bfd8a
Geoffrey Moss <geoffmoss0@gmail.com>
2019-02-26 14:59:16 -0500

    Fixed insert method so it creates a binary searc tree, implemented left_rotate, no testing yet

--- main.c
+++ main.c
@@ -6,6 +6,32 @@
 struct node *root;
 
 /**
+* Rotate the tree left
+* I'm just trusting that this algorithm works because I'm only implementing pseudocode here
+* @param node the node to rotate around
+*/
+void left_rotate(struct node *node) {
+	struct node *other = node->right;
+	node->right = other->left;
+	if (other->left != NULL) {
+		other->left->parent = node;
+	}
+	other->parent = node->parent;
+	if (node->parent == NULL) {
+		root = other; //This operation changes the root of the tree to the right child
+	} else {
+		if (node == (node->parent)->left) {
+			node->parent->left = other;
+		} else {
+			node->parent->right = other;
+		}
+	}
+	other->left = node; //Original node is now the left child of its former right child
+	node->parent = other;
+}
+
+
+/**
 * Recursive function that adds a node (sets parent to the next free node, 
 * gives it a value and color)
 * @param *root pointer to the root node
@@ -13,28 +39,28 @@ struct node *root;
 */
 int insert(struct node *start, struct node *node) {
 	//TODO
-	if (start->left == NULL) {
-		node->parent = start;
-		start->left = node;
-		node->color = 1;
-		printf("Added node with value %d to node with value %d on left\n", node->value, start->value);
-		return 1;
-	} else if (start->right == NULL) {
-		node->parent = start;
-		start->right = node;
-		node->color = 1;
-		printf("Added node with value %d to node with value %d on right\n", node->value, start->value);	
-		return 1;
-	} else {
-		if (insert(start->left, node)) {
-			return 1;
-		} else if (insert(start->right, node)) {
+	if (node->value < start->value) {
+		if (start->left == NULL) {
+			node->parent = start;
+			start->left = node;
+			node->color = 1;
+			printf("Added node with value %d to node with value %d on left\n", node->value, start->value);
 			return 1;
 		} else {
-			return 0;
+			return insert(start->left, node);
+		}
+	
+	} else { //node->value >= start->value
+		if (start->right == NULL) {
+			node->parent = start;
+			start->right = node;
+			node->color = 1;
+			printf("Added node with value %d to node with value %d on right\n", node->value, start->value);	
+			return 1;
+		} else {
+			return insert(start->right, node);
 		}
 	}
-	
 }
 
 int main(void) {
@@ -76,6 +102,5 @@ int main(void) {
 	printf("\n");
 
 	printf("root value: %d\n", root->value);
-	printf("last value: %d\n", root->left->left->value);
 	return 0;
 }
