a3866cb294aa334e50edfcd308345248a369ddde
Geoffrey Moss <geoffmoss0@gmail.com>
2019-02-26 00:28:35 -0500

    Adding to tree recursively works!

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #include "node.h"
 
-const struct node *root;
+struct node *root;
 
 /**
 * Recursive function that adds a node (sets parent to the next free node, 
@@ -11,8 +11,30 @@ const struct node *root;
 * @param *root pointer to the root node
 * @param *node the node to be added
 */
-void insert(struct node *node) {
+int insert(struct node *start, struct node *node) {
 	//TODO
+	if (start->left == NULL) {
+		node->parent = start;
+		start->left = node;
+		node->color = 1;
+		printf("Added node with value %d to node with value %d on left\n", node->value, start->value);
+		return 1;
+	} else if (start->right == NULL) {
+		node->parent = start;
+		start->right = node;
+		node->color = 1;
+		printf("Added node with value %d to node with value %d on right\n", node->value, start->value);	
+		return 1;
+	} else {
+		if (insert(start->left, node)) {
+			return 1;
+		} else if (insert(start->right, node)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
 }
 
 int main(void) {
@@ -24,9 +46,6 @@ int main(void) {
 	int size = (int) strtol(read, &ptr, 10); 
 	printf("Reading %d entries: begin entering data:\n", size); 
 
-	int data[2 * size]; 
-	//Because we'll need at least 2 * ceil(log2(size)) Nil leaf nodes,
-	//We'll oversize the array to twice the size, which will always be enough
 	int count = 0;
 	int x;
 	while(count < size) {
@@ -36,23 +55,27 @@ int main(void) {
 			n->parent = NULL;
 			n->value = x;
 			n->color = 0;
+			n->left = NULL;
+			n->right = NULL;
 			root = n;
-		} 
-		else {
-			struct node n = malloc(sizeof(node));
-			insert(n);
+		} else {
+			struct node *n = malloc(sizeof(node));
+			n->value = x;
+			n->parent = NULL;
+			n->left = NULL;
+			n->right = NULL;
+			if (!insert(root, n)) {
+				printf("Something's gone horribly wrong");
+				exit(1);
+			}
 		}
 		//TODO replace this with the insert and balancing methods
-		//TODO need some way to keep track of the 
-		data[count] = x;
 		count++;
 	}
 
-	for (int i = 0; i < size; i++) {
-		printf("%d, ", data[i]);
-	}
 	printf("\n");
 
 	printf("root value: %d\n", root->value);
+	printf("last value: %d\n", root->left->left->value);
 	return 0;
 }
